219b71dba0df68e4c985facafff75f1d842a4894
Qbeety <beatricenkondola22@gmail.com>
2021-02-20 01:32:20 +0300

    application endpoint testing

--- test/index.ts
+++ test/index.ts
@@ -9,10 +9,34 @@ describe("applicaton testing", () => {
   });
 
   /** */
-  describe("home rote testing", () => {
+  describe("GET /home route testing", () => {
     it("should respond wiht status 200", async () => {
       const result = await request(app).get("/");
       expect(result.statusCode).to.equal(200);
     });
   });
+
+  /** */
+  describe("GET /signup route testing", () => {
+    it("should respond wiht status 200", async () => {
+      const result = await request(app).get("/signup");
+      expect(result.statusCode).to.equal(200);
+    });
+  });
+
+  /** */
+  describe("GET /signin route testing", () => {
+    it("should respond wiht status 200", async () => {
+      const result = await request(app).get("/signin");
+      expect(result.statusCode).to.equal(200);
+    });
+  });
+
+  /** */
+  // describe("GET /ticket route testing", () => {
+  //   it("should respond wiht status 200", async () => {
+  //     const result = await request(app).get("/ticket");
+  //     expect(result.statusCode).to.equal(200);
+  //   });
+  // });
 });
